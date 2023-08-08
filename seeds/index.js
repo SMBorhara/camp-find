277efc61321bc24e5e4146fab0cb831e7e97f352
Shivani Borhara <shivaniborhara@shivanis-mbp.lan>
2023-08-08 01:05:20 -0400

    styling nav footer

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: 'https://source.unsplash.com/collection/483251',
+			description:
+				'Breathe in fresh air and find yourself grounded amongst the green of the tress and rustling of the streams. Be one with the earth and disconnect from the distractions of modern life.',
+			price,
 		});
 		await camp.save();
 	}
